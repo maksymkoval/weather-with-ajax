a90ae82f8889bbda5cd501eaf36ae0a7892d3a7c
maksymkoval <makskoval714@gmail.com>
2019-07-25 16:00:22 +0300

    forth

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ console.log(data);
 weather.append('<div class="wrap">'+
     '<div class="first">location:<b>'+data.name+'</b><br>'+
     ' weather: <b>'+data.weather[0].main+'</b><br>'+
-'<img  class="image" src="http://openweathermap.org/img/w/'+data.weather[0].icon+'.png"><br>'+
+'<img  class="image" src="https://openweathermap.org/img/w/'+data.weather[0].icon+'.png"><br>'+
 'description: <b class="main">'+data.weather[0].description+'</b><br>'+
 'temp: <b class="main">'+data.main.temp+' &#176;C</b><br></div>'+
 '<div class="second">pressure: <b>'+data.main.pressure+' hPa</b><br>'+
